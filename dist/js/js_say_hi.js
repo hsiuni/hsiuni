3006e56dd3fed350e47a1bfcc6b32f5fba0bd473
hsiuni <b0961598750@gmail.com>
2023-03-26 16:55:40 +0800

    js say hi

--- dist/js/js_say_hi.js
+++ dist/js/js_say_hi.js
@@ -13,3 +13,17 @@ say.addEventListener('click', () => {
         response.classList.add('active');
     }
 })
+
+let quit = false;
+
+let ss = quit ? 'Bye' : 'Welcome';
+
+let s2 = '';
+
+if (quit) {
+    s2 = 'Bye';
+} else {
+    s2 = 'Welcome';
+}
+
+console.log(ss, s2);
